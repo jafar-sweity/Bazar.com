2e00a4e3b8757327264e266e60fefd7b91bd3941
Bdair2002 <m.abed.bdair-@hotmail.com>
2024-03-20 22:20:05 +0200

    Added operations to Update service

--- catalog_server/app.js
+++ catalog_server/app.js
@@ -34,11 +34,13 @@ app.get('/catalog_server/query', (req, res) => {
   }
 });
 app.post('/catalog_server/update', (req, res) => {
+  if(req.body.operation==="decrement"){
   data.forEach(book => {
     if (book.id === req.body.id){
       book.stock = book.stock - 1;
       res.status(200).json({ message: 'Purchase successful' ,success:true,book:book});
   }});
+}
 }
 );
 app.listen(PORT, () => {
